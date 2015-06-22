10d43f3af86d2c42bc0d6894f0c865acaa2cee67
Andrey Golovin <andrey@mapbox.com>
2015-06-22 11:51:18 +0300

    added Mapbox and UkrOrtho layers

--- osmtm/static/js/project.edit.js
+++ osmtm/static/js/project.edit.js
@@ -122,10 +122,23 @@ osmtm.project.edit.priority_areas = (function() {
         subdomains: '1234',
         attribution: 'Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contibutors</a>, Tiles &copy; <a href=\"http://www.mapquest.com/\">MapQuest</a>'
         });
+    var Mapbox_Sat = L.tileLayer('http://{s}.tiles.mapbox.com/v4/openstreetmap.map-inh7ifmo/{zoom}/{x}/{y}.png?access_token=pk.eyJ1Ijoib3BlbnN0cmVldG1hcCIsImEiOiJncjlmd0t3In0.DmZsIeOW-3x-C5eX-wAqTw', {
+        name: 'Mapbox Satelite',
+        maxZoom: 17,
+        subdomains: 'abc',
+        attribution: 'Tiles &copy; <a href="http://mapbox.com/">Mapbox</a>'
+        });
+    var ukrortho = L.titleLaayer('http://212.26.144.110/tile2/orto_10000/{zoom}/{x}/{-y}.jpg', {
+        name: 'Україна, ортофотомапи 2012',
+        maxZoom 16,
+        attribution = 'Tiles &copy; <a href="http://map.land.gov.ua/kadastrova-karta">ЦДЗК</a>'
+        });
     var baseLayers = {
         "MapSurfer.NET": MapSurferNET, 
         "OpenStreetMap": OpenStreetMap, 
-        "MapQuest": MapQuest
+        "MapQuest": MapQuest,
+        "Mapbox Satelite": Mapbox_Sat,
+        "Україна, ортофотомапи 2012": urkortho
         };
     var overlays = { };
     lmap.addLayer(OpenStreetMap);
